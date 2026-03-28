3cb0859d1b9ff13a788a2d74792a0acd7b8db1bb
Taruni Medishetty <medishettytaruni@gmail.com>
2026-03-28 12:10:48 +0530

    mar

--- parameters_arguments.py
+++ parameters_arguments.py
@@ -1,5 +1,15 @@
+#1 print name
 def namee(name="taruni"):
     print(name)
 namee()
 
 #2 add 2 nums
+def add_nums(a,b):
+    sum=a+b
+    return sum
+print(add_nums(3,4))
+
+#
+def ex(name="guest"):
+    print("hello"+name)
+ex()
