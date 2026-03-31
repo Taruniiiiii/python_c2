75e802b41415fe52f0fe67c80e095acb2cf586a8
Taruni Medishetty <medishettytaruni@gmail.com>
2026-03-31 20:55:32 +0530

    mar

--- oops.py
+++ oops.py
@@ -1,4 +1,4 @@
-#inheritance
+"""#inheritance(single)
 class manager:
     def show(self):
         print("im a manager")
@@ -8,3 +8,28 @@ class employee(manager):
 e=employee()
 e.show()
 e.intro()
+#multiple inheritance(multiple parent and 1 child)
+class A:
+    def aa(self):
+        print("class A")
+class B:
+    def bb(self):
+        print("class B")
+class C(A,B):
+    def cc(self):
+        print("class C")
+c=C()
+c.aa()
+c.bb()"""
+#multilevel(1 parent multiple child)hierarchial
+class A:
+    def aa(self):
+        print("class A")
+class B(A):
+    def bb(self):
+        print("class B")
+class C(B):
+    def cc(self):
+        print("class C")
+b=B()
+b.bb()
