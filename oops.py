79f450598590f37479e182c066c62dee4845142d
Taruni Medishetty <medishettytaruni@gmail.com>
2026-04-04 23:41:17 +0530

    mar

--- oops.py
+++ oops.py
@@ -98,3 +98,15 @@ class employee(person):
         print("Salary",self.salary)
 e=employee("taruni",5000)
 e.display()    
+
+##leet
+class RecentCounter:
+    def __init__(self):
+        self.records = []
+        self.start = 0
+
+    def ping(self, t: int) -> int:
+        self.records.append(t)
+        while self.records[self.start] < t - 3000:
+            self.start += 1
+        return len(self.records) - self.start
