6b6e4370acd959d2bfe92fda60fe0106999972de
Taruni Medishetty <medishettytaruni@gmail.com>
2026-05-14 12:24:11 +0530

    may

---
+++ regx_filehandling.py
@@ -0,0 +1,21 @@
+import re
+
+#Return a list containing every occurrence of "ai":
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if "Portugal" is in the string:
+
+x = re.findall("Portugal", txt)
+print(x)
+
+if (x):
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
