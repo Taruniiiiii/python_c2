6d99fac5ba1e5dce8e3519850e7a7a7bf5b56eed
Taruni Medishetty <medishettytaruni@gmail.com>
2026-05-14 12:55:19 +0530

    may

--- regx_filehandling.py
+++ regx_filehandling.py
@@ -19,3 +19,32 @@ if (x):
   print("Yes, there is at least one match!")
 else:
   print("No match")
+import re
+
+#Split the string at the first white-space character:
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 2)
+print(x)
+import re
+
+#Replace all white-space characters with the digit "9":
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+import re
+
+txt = "The rain in2839010 Spain"
+
+#Check if the string has any 0, 1, 2, or 3 digits:
+
+x = re.findall("[0123]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
