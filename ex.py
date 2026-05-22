16c5a7f06d0aea5f8c743b4c340957f5cdc11fa6
Taruni Medishetty <medishettytaruni@gmail.com>
2026-05-22 23:40:40 +0530

    may

--- ex.py
+++ ex.py
@@ -191,3 +191,11 @@ class Solution:
                 num=num-1
             step_count+=1
         return step_count
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum=nums[0]
+        max_sum=nums[0]
+        for num in nums[1:]:
+            current_sum=max(num,current_sum+num)
+            max_sum=max(max_sum,current_sum)
+        return max_sum
