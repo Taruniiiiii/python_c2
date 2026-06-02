782d0e0fd365d13fb4a5c6afd27642fc6129d506
Taruni Medishetty <medishettytaruni@gmail.com>
2026-06-02 20:44:46 +0530

    jul

--- ex.py
+++ ex.py
@@ -316,3 +316,11 @@ class Solution:
             res.append(flat[i:i+c])
         return res
 
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count=0
+        for row in grid:
+            for i in row:
+                if i<0:
+                    count+=1
+        return count        
