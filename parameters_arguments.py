675e1e2285c0b46e25d81b0b68bf0a7ba8426a53
Taruni Medishetty <medishettytaruni@gmail.com>
2026-06-05 22:06:38 +0530

    jun

--- parameters_arguments.py
+++ parameters_arguments.py
@@ -155,3 +155,12 @@ def bonAppetit(bill, k, b):
         print("Bon Appetit")
     else:
         print(b-actual_bill)
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(len(heights)):
+            for j in range(len(heights)-i-1):
+                if heights[j]<heights[j+1]:
+                    heights[j],heights[j+1]=heights[j+1],heights[j]
+                    names[j],names[j+1]=names[j+1],names[j]
+        return names
+        
