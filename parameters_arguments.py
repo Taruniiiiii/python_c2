db77faf5e0847b4d122c25a8d062c4312e015088
Taruni_Medishetty <158485758+Taruniiiiii@users.noreply.github.com>
2026-06-09 23:51:54 +0530

    Update parameters_arguments.py

--- parameters_arguments.py
+++ parameters_arguments.py
@@ -2,12 +2,12 @@
 def namee(name="taruni"):
     print(name)
 namee()
-"""
+
 #2 add 2 nums
 def add_nums(a,b):
     sum=a+b
     return sum
-print(add_nums(3,4))
+print(add_nums(3,4))"""
 
 #3If no name is given, print "Guest
 def ex(name="guest"):
