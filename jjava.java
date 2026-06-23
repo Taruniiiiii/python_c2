76e358d9776acf8a01333d6122951da12c1fefdd
Taruni Medishetty <medishettytaruni@gmail.com>
2026-06-23 15:02:52 +0530

    java training

--- jjava.java
+++ jjava.java
@@ -33,5 +33,58 @@ public class Program1
 }
 System.out.println
 
+package com.taruni;
+
+public class Animal {
+	String name;
+	int legs;
+	String color;
+	
+	public void eat() {
+		System.out.println("all the dogs bark");
+	}
+	public void sleep() {
+		System.out.println("all the dogs sleep");
+		
+	}
+
+
+}
+
+class Dogs extends Animal
+{
+	String breed;
+	int age;
+	
+	public void toBark()
+	{
+		System.out.println("all the dogs barks");
+	}
+	
+	public void toProtect()
+	{
+		System.out.println("all the dogs protect");
+	}
+
+		
+}
+class Mains
+{
+	public static void main(String[] args)
+	{
+		Dogs d=new Dogs();
+		System.out.println(d.age);
+		System.out.println(d.name);
+		System.out.println(d.color);
+		System.out.println(d.breed);
+		System.out.println(d.legs);
+		
+		d.toBark();
+		d.toProtect();
+		d.sleep();
+		d.eat();
+	}
+}
+
 
 
