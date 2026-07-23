9732c5fa18e20f383c336aae87060eb0d2dc6263
Taruni Medishetty <medishettytaruni@gmail.com>
2026-07-23 23:11:32 +0530

    leet

--- sort,search.py
+++ sort,search.py
@@ -708,6 +708,16 @@ class Solution:
                 if product>maxi:
                     maxi=product
         return maxi
+#leet
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
 
 
 
